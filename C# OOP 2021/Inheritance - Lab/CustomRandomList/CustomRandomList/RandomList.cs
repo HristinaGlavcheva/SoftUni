4d59b0b24196d4bea21794bf93f8841166c35f0c
Hristina Glavcheva <glavchevahristina@gmail.com>
2021-01-21 15:15:16 +0200

    Commit RandomList

---
+++ C# OOP 2021/Inheritance - Lab/CustomRandomList/CustomRandomList/RandomList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private Random random = new Random();
+
+        public string RandomString()
+        {
+            var randomIndex = this.random.Next(0, this.Count);
+            var randomElement = this[randomIndex];
+            this.RemoveAt(randomIndex);
+            return randomElement;
+        }
+    }
+}
