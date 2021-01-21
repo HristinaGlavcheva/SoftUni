badeda3c48a8451fb517a01d01b1c6a88cc69d22
Hristina Glavcheva <glavchevahristina@gmail.com>
2021-01-21 16:34:23 +0200

    Commint Person

---
+++ C# OOP 2021/Inheritance - Exercise/Person/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Person
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                string name = Console.ReadLine();
+                int age = int.Parse(Console.ReadLine());
+
+                Person person;
+
+                if(age >= 15)
+                {
+                    person = new Person(name, age);
+                }
+                else
+                {
+                    person = new Child(name, age);
+
+                }
+
+                Console.WriteLine(person);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
