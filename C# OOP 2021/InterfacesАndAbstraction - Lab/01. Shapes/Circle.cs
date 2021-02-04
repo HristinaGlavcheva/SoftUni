64a2ceb871d0ca047c6c22b37b91ac71845ecb5a
Hristina Glavcheva <glavchevahristina@gmail.com>
2021-02-04 15:00:22 +0200

    Commit

---
+++ C# OOP 2021/InterfacesАndAbstraction - Lab/01. Shapes/Circle.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace _01._Shapes
+{
+    public class Circle : IDrawable
+    {
+        private int radius;
+
+        public Circle(int radius)
+        {
+            this.radius = radius;
+        }
+
+        public void Draw()
+        {
+            //for (int x = 0; x <= 2 * this.radius; x++)
+            //{
+            //    for (int y = 0; y <= 2 * this.radius; y++)
+            //    {
+            //        var distance = Math.Ceiling(Math.Sqrt((radius - x) * (radius - x) + (radius - y) * (radius - y)));
+
+            //        if (distance == radius)
+            //        {
+            //            Console.Write("**");
+            //        }
+            //        else
+            //        {
+            //            Console.Write("  ");
+            //        }
+            //    }
+
+            //    Console.WriteLine();
+            //}
+
+            double rIn = this.radius - 0.4;
+            double rOut = this.radius + 0.4;
+
+            for (double y = this.radius; y >= -this.radius; --y)
+            {
+                for (double x = -this.radius; x < rOut; x += 0.5)
+                {
+                    double value = x * x + y * y;
+
+                    if (value >= rIn * rIn && value <= rOut * rOut)
+
+                        Console.Write("*");
+
+                    else
+
+                        Console.Write(" ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ C# OOP 2021/InterfacesАndAbstraction - Lab/02. Cars/Tesla.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02._Cars
+{
+    public class Tesla : IElectricCar
+    {
+        public Tesla(string model, string color, int battery)
+        {
+            this.Model = model;
+            this.Color = color;
+            this.Battery = battery;
+        }
+
+        public int Battery { get; private set; }
+
+        public string Model { get; private set; }
+
+        public string Color { get; private set; }
+
+        public string Start()
+        {
+            return "Engine start"; 
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{this.Color} {this.GetType().Name} {this.Model} with {this.Battery} Batteries");
+            sb.AppendLine(this.Start());
+            sb.AppendLine(this.Stop());
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
