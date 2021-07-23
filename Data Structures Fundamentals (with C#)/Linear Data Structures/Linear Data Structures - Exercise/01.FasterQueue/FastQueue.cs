2e346f090ee36ed543b303edf209089e945822a3
Hristina Glavcheva <glavchevahristina@gmail.com>
2021-07-24 00:24:52 +0300

    Implement fast queue

--- Data Structures Fundamentals (with C#)/Linear Data Structures/Linear Data Structures - Exercise/01.FasterQueue/FastQueue.cs
+++ Data Structures Fundamentals (with C#)/Linear Data Structures/Linear Data Structures - Exercise/01.FasterQueue/FastQueue.cs
@@ -6,37 +6,87 @@
 
     public class FastQueue<T> : IAbstractQueue<T>
     {
-        private Node<T> _head;
+        private Node<T> head;
+        private Node<T> tail;
+
         public int Count { get; private set; }
 
         public bool Contains(T item)
         {
-            throw new NotImplementedException();
+            var current = this.head;
+
+            while (current != null)
+            {
+                if (current.Item.Equals(item))
+                {
+                    return true;
+                }
+                current = current.Next;
+            }
+
+            return false;
         }
 
         public T Dequeue()
         {
-            throw new NotImplementedException();
+            this.EnsureNotEmpty();
+
+            var oldHead = this.head;
+            this.head = this.head.Next;
+
+            this.Count--;
+
+            return oldHead.Item;
         }
 
         public void Enqueue(T item)
         {
-            throw new NotImplementedException();
+            var newNode = new Node<T>(item);
+            
+            if(this.Count == 0)
+            {
+                this.head = newNode;
+                this.tail = newNode;
+            }
+            else
+            {
+                var oldTail = this.tail;
+                this.tail = newNode;
+                oldTail.Next = tail;
+            }
+
+            this.Count++;
         }
 
         public T Peek()
         {
-            throw new NotImplementedException();
+            this.EnsureNotEmpty();
+
+            return this.head.Item;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            var current = this.head;
+
+            while (current != null)
+            {
+                yield return current.Item;
+                current = current.Next;
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return this.GetEnumerator();
+        }
+
+        private void EnsureNotEmpty()
+        {
+            if(this.Count == 0)
+            {
+                throw new InvalidOperationException("The queue is empty!");
+            }
         }
     }
 }
--- Data Structures Fundamentals (with C#)/Linear Data Structures/Linear Data Structures - Exercise/01.FasterQueue/Node.cs
+++ Data Structures Fundamentals (with C#)/Linear Data Structures/Linear Data Structures - Exercise/01.FasterQueue/Node.cs
@@ -2,6 +2,11 @@
 {
     public class Node<T>
     {
+        public Node(T item)
+        {
+            this.Item = item;
+        }
+
         public T Item { get; set; }
 
         public Node<T> Next { get; set; }
