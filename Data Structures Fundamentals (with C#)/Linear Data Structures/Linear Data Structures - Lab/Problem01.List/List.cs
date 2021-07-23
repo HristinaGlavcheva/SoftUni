dfc94aa58638c1dfef55307f8889448fba65b640
Hristina Glavcheva <glavchevahristina@gmail.com>
2021-07-23 13:27:00 +0300

    Dynamic implementation of list, linked list and stack

---
+++ Data Structures Fundamentals (with C#)/Linear Data Structures/Linear Data Structures - Lab/Problem01.List/List.cs
@@ -0,0 +1,149 @@
+﻿namespace Problem01.List
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class List<T> : IAbstractList<T>
+    {
+        private const int DEFAULT_CAPACITY = 4;
+        private T[] _items;
+
+        public List()
+            : this(DEFAULT_CAPACITY) {
+        }
+
+        public List(int capacity)
+        {
+            this._items = new T[capacity];
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                this.ValidateIndex(index);
+                return this._items[index];
+            }
+            set
+            {
+                this.ValidateIndex(index);
+                this._items[index] = value;
+            }
+        }
+
+        public int Count { get; private set; }
+
+        public void Add(T item)
+        {
+            this.EnsureCapacity();
+
+            this._items[this.Count] = item;
+            this.Count++;
+        }
+
+        public bool Contains(T item)
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                if (this._items[i].Equals(item))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+
+        public int IndexOf(T item)
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                if (this._items[i].Equals(item))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public void Insert(int index, T item)
+        {
+            this.ValidateIndex(index);
+            this.EnsureCapacity();
+            this.Count++;
+
+            for (int i = this.Count; i > index; i--)
+            {
+                this._items[i] = this._items[i - 1];
+            }
+
+            this._items[index] = item;
+        }
+
+        public bool Remove(T item)
+        {
+            if (!this.Contains(item))
+            {
+                return false;
+            }
+
+            int index = this.IndexOf(item);
+            this.RemoveAt(index);
+
+            return true;
+        }
+
+        public void RemoveAt(int index)
+        {
+            this.ValidateIndex(index);
+
+            for (int i = index; i < this.Count - 1; i++)
+            {
+                this._items[i] = this._items[i + 1];
+            }
+
+            this._items[this.Count - 1] = default;
+            this.Count--;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                yield return this._items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() 
+            => this.GetEnumerator();
+
+        private void EnsureCapacity()
+        {
+            if (this.Count < this._items.Length)
+            {
+                return;
+            }
+
+            T[] doubledSizeArray = new T[this._items.Length * 2];
+
+            for (int i = 0; i < this._items.Length; i++)
+            {
+                doubledSizeArray[i] = this._items[i];
+            }
+            //Array.Copy(this.array, doubledSizeArray, this.array.Length);
+
+            this._items = doubledSizeArray;
+        }
+
+        private void ValidateIndex(int index)
+        {
+            if (index < 0 || index >= this.Count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
+    }
+}
